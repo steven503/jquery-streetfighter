c1d59c71f6e6863ab6441f882091900344d0abb1
Steven Filler <sfiller@mac.com>
2016-01-07 19:48:14 -0600

    implement mousedown and mouseup functions

--- js/app.js
+++ js/app.js
@@ -6,5 +6,16 @@ $(document).ready(function() {
   .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // show hadouken and animate it
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 });
